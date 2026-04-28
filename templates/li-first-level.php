4dcd94894fc72e082fb38026de52bc8bc0756746
Tonny Santana <tonnysantanasantos@gmail.com>
2026-04-28 17:08:37 -0300

    feat: add first level button template for mobile menu

---
+++ templates/li-first-level.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * First Level Button Template
+ *
+ * @package AARDEX
+ * @param $args Array with the template variables.
+ */
+
+?>
+<li>
+
+	<button class="aardex-mobile-menu--first_level-button" >
+		<?php echo esc_html( $args['label'] ); ?>
+		<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M6 12L10 8L6 4" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+		</svg>
+	</button>
+
+</li>
